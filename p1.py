6e9f76b1baa67f18faae80b778fa77f6e6e3f7fd
VerDenVlad <dvnainternet@gmail.com>
2023-10-08 03:13:03 +0300

    Угадай число

--- p1.py
+++ p1.py
@@ -1,1 +1,11 @@
-Проба
+# Угадай число
+import random
+
+rand_num = random.randint(1, 5)
+user_num = input("Угадай число (от 1 до 5):")
+
+if user_num == rand_num:
+    print("Вы угадали!")
+else:
+    print("Вы не угадали :()")
+    print(f"Было загадано число {rand_num}")
